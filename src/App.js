62bd42d9ee6ef91697f165e9c2e2fc501d37af5e
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-08-02 22:27:44 +0300

    bug

--- src/App.js
+++ src/App.js
@@ -1,65 +1,106 @@
-import react from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-
-class App extends react.Component {
-
-
+import './App.css'
+import React from 'react'
+import axios from 'axios'
+class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state= {
-      locationName : '',
-      allInfo : {},
-      showData : false,
-
+    this.state = {
+      locationResult: {},
+      searchQuery: '',
+      show : true,
+      errorMsg: false,
+      weatherData: [],
+      errorMsgWeather: false,
+      showWeather: false
     }
   }
+  getLocFun = async (e) => {
+    e.preventDefault()
+    console.log('inside getLocFun')
+    let reqUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`
 
-  viewLocation = async (event) => {
+    try{
+      let locRes = await axios.get(reqUrl)
+      console.log(locRes.data)
+      this.setState({
+        locationResult: locRes.data[0],
+      })
+      console.log(this.state.locationResult.lat, this.state.locationResult.lon)
+    }catch{
+      this.setState({
+        show : false,
+        errorMsg : true
+      })
+    }
 
-    event.preventDefault();
-    await this.setState({locationName:event.target.Cityname.value});
-    let url = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_API_KEY}&q=${this.state.locationName}&format=json`;
-    let response =await axios.get(url);
-    console.log(response);
-    this.setState({allInfo:response.data[0],showData:true})
 
+
+
+    this.displayWeather()
+  }
+  displayWeather = async () => {
+    try{
+      const weather = await axios.get(`${process.env.REACT_APP_SERVER}/weather`, {
+      params: { searchQuery: this.state.searchQuery }
+    })
+    console.log(weather)
+    this.setState({
+      weatherData: weather.data,
+      showWeather : true
+    })
+  }catch{
+    this.setState({
+      errorMsgWeather: true,
+      showWeather : false
+
+    })
   }
 
-
-  render() {
-    return (
-      <div>
-               <h1>Weclome my app aplication</h1>
-
-
-        <Form onSubmit={this.viewLocation}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="text" name="Cityname" placeholder="Enter Place" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-              submit
-          </Button>
-        </Form>
-      <br>
-      </br>
-      {this.state.showData &&
-      <>
-      <p> Place Name : {this.state.allInfo.display_name}</p>
-      <p> Latitude : {this.state.allInfo.lat}</p>
-      <p> Longitude :{this.state.allInfo.lon}</p>
-      <br>
-    </br>
-    <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_API_KEY}&center=${this.state.allInfo.lat},${this.state.allInfo.lon}&zoom=10`} alt='here we are'/>
-    </>
-
-      }
-
-      </div>
-    )
-  }
+}
+updateSearchQuery = (e) => {
+  this.setState({
+    searchQuery: e.target.value
+  })
 }
 
+render() {
+
+  return (
+    <div>
+      <h3>City Explorer app</h3>
+
+      <form onSubmit={this.getLocFun}>
+        <input  onChange={this.updateSearchQuery} type="text" name="city" />
+        <input type="submit" value='"Explore!' />
+      </form>
+
+      <p>City name: {this.state.locationResult.display_name}</p>
+      <p>latitude: {this.state.locationResult.lat}</p>
+      <p>latitude: {this.state.locationResult.lon}</p>
+
+      {this.state.show &&
+        <img
+        src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center= ${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=18`}
+        alt="city"
+        />
+      }
+
+      {this.state.errorMsg &&
+      <p>Error in getting location data</p>}
+
+      {this.state.showWeather &&
+        this.state.weatherData.map((day, index) => (
+        <div key={index}>
+          <p>day: {day.date}</p>
+          <p>description: {day.description}</p>
+        </div>
+
+      ))}
+
+      {this.state.errorMsgWeather &&
+      <p>Error in getting the weather data</p>}
+    </div>
+  )
+  }
+}
 export default App;
